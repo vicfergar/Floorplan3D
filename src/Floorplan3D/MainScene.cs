daaea815863571c3f46bdea0b706eb5f054e85c1
Victor Ferrer <victorfergar@gmail.com>
2022-10-23 13:09:59 +0200

    feat: Replace teapot with a ground plane

--- src/Floorplan3D/MainScene.cs
+++ src/Floorplan3D/MainScene.cs
@@ -1,4 +1,8 @@
+using Evergine.Common.Graphics;
+using Evergine.Components.Graphics3D;
 using Evergine.Framework;
+using Evergine.Framework.Graphics;
+using Evergine.Framework.Graphics.Materials;
 
 namespace Floorplan3D
 {
@@ -12,6 +16,27 @@ namespace Floorplan3D
 
         protected override void CreateScene()
         {
+            var defaultMaterial = Managers.AssetSceneManager.Load<Material>(EvergineContent.Materials.DefaultMaterial);
+            var groundMaterial = new StandardMaterial(defaultMaterial.Clone())
+            {
+                BaseColor = Color.LightGray,
+                AlphaCutout = 0,
+            };
+
+            var ground = new Entity("ground")
+                .AddComponent(new Transform3D())
+                .AddComponent(new MaterialComponent()
+                { 
+                    Material = groundMaterial.Material
+                })
+                .AddComponent(new PlaneMesh() 
+                {
+                    Width = 4,
+                    Height = 4,
+                    TwoSides = true,
+                })
+                .AddComponent(new MeshRenderer());
+            this.Managers.EntityManager.Add(ground);
         }
     }
 }
