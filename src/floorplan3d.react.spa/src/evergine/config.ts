d03c4985d15976f628d439acb30dc90174b065f2
Victor Ferrer <victorfergar@gmail.com>
2025-03-31 08:13:35 +0200

    chore: Remove WebReact solution

--- src/floorplan3d.react.spa/src/evergine/config.ts
+++
@@ -1,4 +0,0 @@
-export const EVERGINE_CANVAS_ID = "evergine-canvas";
-export const EVERGINE_LOADING_BAR_ID = "evergine-loading-bar";
-export const EVERGINE_ASSEMBLY_NAME = "Floorplan3D.WebReact";
-export const EVERGINE_CLASS_NAME = `${EVERGINE_ASSEMBLY_NAME}.WebFacade`;
