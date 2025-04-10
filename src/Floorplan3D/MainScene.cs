b2da9bdc866567b5d657df90e34925fc3eea6ecf
Victor Ferrer <victorfergar@gmail.com>
2025-04-10 19:12:46 +0200

    chore: Remove unused library Evergine.Bullet

--- src/Floorplan3D/MainScene.cs
+++ src/Floorplan3D/MainScene.cs
@@ -11,9 +11,6 @@ namespace Floorplan3D
         public override void RegisterManagers()
         {
             base.RegisterManagers();
-            
-            this.Managers.AddManager(new global::Evergine.Bullet.BulletPhysicManager3D());
-            
         }
 
         protected override void CreateScene()
